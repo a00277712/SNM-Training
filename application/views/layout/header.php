5250d01e11a3dc2c380cd1136f9be3fa1254d36b
StewartLocke <StewartLocke@MAPS025>
2020-03-13 13:02:12 +0000

    Header banner

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -9,30 +9,33 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="<?=site_url('home')?>">SNM Training</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+      <div class="banner-img">
+        <img src="<?php echo base_url(); ?>img/banner.webp" alt="banner">
+      </div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="<?=site_url('home')?>">SNM Training</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?=site_url('home')?>">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?=site_url('contact')?>">Contact Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?=site_url('about')?>">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?=site_url('report')?>">Report</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?=site_url('admin')?>">Admin</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?=site_url('home')?>">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?=site_url('contact')?>">Contact Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?=site_url('about')?>">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?=site_url('report')?>">Report</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?=site_url('admin')?>">Admin</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
