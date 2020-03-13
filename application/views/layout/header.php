cb2e7a42236c086b5b943e58bd5433c36abd6aa8
StewartLocke <StewartLocke@MAPS025>
2020-03-13 09:30:11 +0000

    All existing content

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -10,7 +10,7 @@
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="index.php">Navbar</a>
+  <a class="navbar-brand" href="<?=site_url('home')?>">SNM Training</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -18,19 +18,19 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="<?=site_url('home')?>">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="contact.php">Contact Us</a>
+        <a class="nav-link" href="<?=site_url('contact')?>">Contact Us</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="about.php">About Us</a>
+        <a class="nav-link" href="<?=site_url('about')?>">About Us</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="report.php">Report</a>
+        <a class="nav-link" href="<?=site_url('report')?>">Report</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="admin.php">Admin</a>
+        <a class="nav-link" href="<?=site_url('admin')?>">Admin</a>
       </li>
     </ul>
   </div>
