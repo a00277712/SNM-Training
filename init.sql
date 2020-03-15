283604baa381e3235163c49b3923d5b18504bfb3
a00277712 <a00277712@student.ait.ie>
2020-03-15 20:47:28 +0000

    improve table

--- init.sql
+++ init.sql
@@ -4,8 +4,16 @@ use snm_training;
 
 create table Feedback (
 Id INT AUTO_INCREMENT PRIMARY KEY,
+DateCreated DateTime NOT NULL,
+Title varchar(30) NULL,
 FullName varchar(30) NOT NULL, 
 Email varchar(30) NOT NULL,
 Phone varchar(30) NOT NULL,
-UserComment varchar(512) NOT NULL
+UserComment varchar(512) NOT NULL,
+Priority varchar(30) NULL,
+IssueStatus varchar(30) NULL
 )
+
+CREATE TRIGGER DateCreated BEFORE INSERT ON  Feedback 
+FOR EACH ROW 
+SET NEW.DateCreated = NOW();
